617e266beaac78ecb0ebb7354576ebe88e608aee
OlgaZhivaeva <olga.zhiv@yandex.ru>
2024-05-29 23:31:36 +0300

    Publish photo to Telegram channel

--- space_photos_bot.py
+++ space_photos_bot.py
@@ -9,4 +9,7 @@ TELEGRAM_TOKEN = os.environ["TELEGRAM_TOKEN"]
 bot = telegram.Bot(token=TELEGRAM_TOKEN)
 print(bot.get_me())
 
-bot.send_message(chat_id="@dvmn_flood", text="Hi! I'm a bot. I can download photos of space from the Internet.")
+
+bot.send_message(chat_id="@space_and_space", text="Hi! I'm a bot. I can download photos of space from the Internet.")
+bot.send_document(chat_id="@space_and_space", document=open('images/spacex_1.jpg', 'rb'))
+bot.send_document(chat_id="@space_and_space", document='https://apod.nasa.gov/apod/image/2107/LRVBPIX3M82Crop1024.jpg')
