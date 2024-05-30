65c182bea2781f9edc9e3825a43ff5d68c67d525
OlgaZhivaeva <olga.zhiv@yandex.ru>
2024-05-31 01:30:12 +0300

    One file publish

--- space_photos_bot.py
+++ space_photos_bot.py
@@ -1,15 +1,41 @@
+import argparse
 import os
-from dotenv import load_dotenv
+import random
 import telegram
+from dotenv import load_dotenv
 
 
 load_dotenv()
 TELEGRAM_TOKEN = os.environ["TELEGRAM_TOKEN"]
+ID_CHAT = os.environ["ID_CHAT"]
+POST_FREQUENSY = os.getenv('POST_FREQUENSY', 14400)
 
-bot = telegram.Bot(token=TELEGRAM_TOKEN)
-print(bot.get_me())
+
+def get_path_to_photo():
+    """Функция для получения пути до фотографии из командной строки"""
+    parser = argparse.ArgumentParser(description='Публикация фотографий космоса')
+    parser.add_argument('-p', '--path_to_photo', help='Путь до фотографии')
+    args = parser.parse_args()
+    return args.path_to_photo
 
 
-bot.send_message(chat_id="@space_and_space", text="Hi! I'm a bot. I can download photos of space from the Internet.")
-bot.send_document(chat_id="@space_and_space", document=open('images/spacex_1.jpg', 'rb'))
-bot.send_document(chat_id="@space_and_space", document='https://apod.nasa.gov/apod/image/2107/LRVBPIX3M82Crop1024.jpg')
+def main():
+    bot = telegram.Bot(token=TELEGRAM_TOKEN)
+    path_to_photo = get_path_to_photo()
+
+    if path_to_photo is None:
+        if os.path.isdir('images'):
+            _, _, image_list = list(os.walk('images'))[0]
+            path_to_photo = f'images/{random.choice(image_list)}'
+            bot.send_document(chat_id=ID_CHAT, document=open(path_to_photo, 'rb'))
+        else:
+            print('There is no catalog with photos. Upload photos.')
+    else:
+        if os.path.isfile(path_to_photo):
+            bot.send_document(chat_id=ID_CHAT, document=open(path_to_photo, 'rb'))
+        else:
+            print('The file does not exist. Specify the correct file path.')
+
+
+if __name__ == "__main__":
+    main()
